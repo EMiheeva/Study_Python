89af3076296422ea8fc6dcfff44f95398ea93461
EMiheeva <90220808+EMiheeva@users.noreply.github.com>
2023-02-16 21:52:10 +0200

    Create main.py

---
+++ Sololearn_Intermediate_Python/Collection Types/2.2 Dictionaries. Practice/main.py
@@ -0,0 +1,14 @@
+"""
+Car Data
+You are working at a car dealership and store the car data in a dictionary.
+Your program needs to take the key as input and output the corresponding value.
+"""
+
+car = {
+    'brand':'BMW',
+    'year': 2018,
+    'color': 'red',
+    'mileage': 15000
+}
+user_key = input()
+print(car[user_key])
