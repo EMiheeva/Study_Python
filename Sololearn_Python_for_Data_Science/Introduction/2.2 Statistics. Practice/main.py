347f033ad439a279919c5fd156cb262ea45f024d
EMiheeva <90220808+EMiheeva@users.noreply.github.com>
2023-02-18 20:18:32 +0200

    Create main.py

---
+++ Sololearn_Python_for_Data_Science/Introduction/2.2 Statistics. Practice/main.py
@@ -0,0 +1,20 @@
+"""
+Number of Vaccinations
+
+We have a report on the number of flu vaccinations in a class of 20 people.
+It has the following numbers:
+never: 5
+once: 8
+twice: 4
+3 times: 3
+What is the mean number of times those people have been vaccinated?
+Output the result using the print() statement.
+Hint: Think about the data this way: it contains 20 values, each representing the number of vaccinations the corresponding person had.
+"""
+
+vac_nums = [0,0,0,0,0,
+            1,1,1,1,1,1,1,1,
+            2,2,2,2,
+            3,3,3
+            ]
+print((0*5+8*1+4*2+3*3)/20)
