a681fc615dc5bba06aba85c7325f50da7ffd973a
EMiheeva <90220808+EMiheeva@users.noreply.github.com>
2023-04-15 17:24:15 +0200

    Create main.py

---
+++ Sololearn_Intermediate_Python/Functional Programming/15.2 Decorators. Practice/main.py
@@ -0,0 +1,20 @@
+"""
+Decorators
+
+You are working on an invoicing system.
+The system has an already defined invoice() function, which takes the invoice number as argument and outputs it.
+You need to add a decorator for the invoice() function, that will print the invoice in the following format:
+"""
+def decor(func):
+    def wrap(num):
+        print("***")
+        func(num)
+        print("***")
+        print("END OF PAGE")
+    return wrap
+
+@decor
+def invoice(num):
+    print("INVOICE #" +num)
+
+invoice(input());
