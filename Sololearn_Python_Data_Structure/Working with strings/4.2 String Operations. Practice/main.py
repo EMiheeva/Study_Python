321b39ca2abdadcbd7666bb3a14c43cb11245d8d
EMiheeva <90220808+EMiheeva@users.noreply.github.com>
2023-04-16 13:10:39 +0200

    Create main.py

---
+++ Sololearn_Python_Data_Structure/Working with strings/4.2 String Operations. Practice/main.py
@@ -0,0 +1,16 @@
+# My variant
+string = input()
+count = 0
+for i in string:
+    print(i*(count+1))
+    count+=1
+"""
+Sololearn variant
+word = input()
+print()
+i = 0
+while (i < len(word)):
+  print(word[i]*(i+1))
+  i+=1 
+  #i++ - habit from C++ syntax
+ """
