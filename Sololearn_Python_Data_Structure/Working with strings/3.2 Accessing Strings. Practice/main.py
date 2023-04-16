be24c4b3b36394194f18615ccc683d92d8a21ca6
EMiheeva <90220808+EMiheeva@users.noreply.github.com>
2023-04-16 13:23:35 +0200

    Update main.py

--- Sololearn_Python_Data_Structure/Working with strings/3.2 Accessing Strings. Practice/main.py
+++ Sololearn_Python_Data_Structure/Working with strings/3.2 Accessing Strings. Practice/main.py
@@ -1,4 +1,9 @@
 """
+You need to make a program that counts the number of vowels in a given text.
+The vowels are a, e, i, o, and u.
+
+Take a string as input and output the number of vowels.
+
 Sololearn variant
 i = input()
 count = 0
